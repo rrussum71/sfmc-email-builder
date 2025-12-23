88a32e882b5a1e9ee4d6b80af44d4ac61637cd12
Richard Russum <rrussum@rodanandfields.com>
2025-12-23 08:29:48 -0800

    Updates

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/sfmc-email-builder/', // REQUIRED for GitHub Pages
-})
